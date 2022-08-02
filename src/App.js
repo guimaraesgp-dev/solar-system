33e116743194f0a568d91b5f43bdbb9e59cfbe1b
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-08-02 00:18:48 -0300

    FEITO

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
-import Missions from './data/missions';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <div><header className="header"><h1> Sistema Solar </h1></header></div>;
+    return (
+      <header className="header">
+        <h1>Sistema Solar</h1>
+      </header>
+    );
   }
 }
 
